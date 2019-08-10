445391c16bbfc51fccca6d2b9fa6793596e78762
KD Technology <34989581+yuvalkedar@users.noreply.github.com>
2019-08-10 17:20:23 +0300

    Update bt_matrix.ino

--- bt_matrix/bt_matrix.ino
+++ bt_matrix/bt_matrix.ino
@@ -3,12 +3,12 @@
 #include "fonts/MyHeb5x7.h"
 
 // #define USE_DOUBLE_BUFFER		// Use if you use a lot of graphics... if comment so comment AUTO_UPDATE_BUFFER too!
-#define WIDTH					(16)
-#define HEIGHT					(8)
+#define WIDTH                  (16)
+#define HEIGHT                 (8)
 #define DISPLAY_MONOCOLOR
-#define COMMAND_END_CHARACTER	('\x0D')
-#define TEXT_BUFFER_SIZE		(40)
-#define SERIAL_BUFFER_SIZE		(TEXT_BUFFER_SIZE + 5)
+#define COMMAND_END_CHARACTER  ('\x0D')
+#define TEXT_BUFFER_SIZE       (40)
+#define SERIAL_BUFFER_SIZE     (TEXT_BUFFER_SIZE + 5)
 
 SoftwareSerial bluetooth_serial(3, 2); // RX, TX
 
@@ -22,12 +22,12 @@ uint32_t text_scroll_ms = 80;
 uint32_t old_text_scroll_ms;
 
 
-void refresh_text(){
+void refresh_text() {
 	fillDisplay(BLACK_C);
 	text_steps = scrollMsg(MSG_A, text_buffer, direction, 0, 0, (uint8_t *)MyHeb5x7, 1, RED_C, BLACK_C);
 }
 
-void setup(){
+void setup() {
 	bluetooth_serial.begin(9600);
 	bluetooth_serial.setTimeout(10);
 	
@@ -38,14 +38,14 @@ void setup(){
 	refresh_text();
 }
 
-void loop(){
+void loop() {
 	// uint8_t read_bytes_count = bluetooth_serial.readBytesUntil(COMMAND_END_CHARACTER, serial_buffer, SERIAL_BUFFER_SIZE);
 	uint8_t read_bytes_count = bluetooth_serial.readBytes(serial_buffer, SERIAL_BUFFER_SIZE);
 	uint32_t ms = millis();
-	if(read_bytes_count){
+	if (read_bytes_count) {
 		// serial_buffer[read_bytes_count - 1] = NULL;
 		serial_buffer[read_bytes_count++] = NULL;		//	do for me the COMMAND_END_CHARACTER
-		switch(serial_buffer[0]){
+		switch (serial_buffer[0]) {
 			case 't':
 			case 'T':
 				memcpy(text_buffer, serial_buffer + 2, read_bytes_count - 1);	// copies read_bytes_count-1 number of characters from serial_buffer to text_buffer
@@ -84,9 +84,9 @@ void loop(){
 		}
 	}
 	/* scroll the text one pixel at a time */
-	if((ms - old_text_scroll_ms) > text_scroll_ms){
+	if ((ms - old_text_scroll_ms) > text_scroll_ms) {
 		old_text_scroll_ms = ms;
-		if(text_steps--)
+		if (text_steps--)
 			moveMsg(MSG_A);
 		else
 			refresh_text();
